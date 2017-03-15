daad846f2dbf8d480526372e2326027da8f47b91
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-03-15 13:56:19 -0600

    feat(core) Add new README file

---
+++ README.md
@@ -0,0 +1,11 @@
+#Apple Creek Farm - Bowdouinham, ME
+
+This is a webpage for Apple Creek Farm, located in Bowdouinham Maine.  
+
+###To-Do
+
+* Add a sweet font from this page
+* Add background images for each section
+* Add parallax scrolling to see how it looks
+* Add and populate separate pages
+* Do some sort of server magic to make the urls come out great and be searchable.
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,34 @@
 
 // require('../styles/main.css');
 
-console.log('dicks');
+console.log("I'm in here!")
 
-const navigate = path => {
-  const current = window.location.href
-  window.location.href = current.replace()
+const getCurrentUrl = () => window.location.pathname
+
+// console.log('location:', window.location);
+
+const listen = () => {
+  const current = getCurrentUrl()
+  const filePath = /\pages\//
+  const fileExtension = /\.html/
+  if (current.includes('pages/')) {
+    const newUrl = current
+    .replace(filePath, '')
+    .replace(fileExtension, '')
+    console.log('new url:', newUrl);
+    // window.location.pathname  = newUrl
+  }
+
 }
+
+listen()
+
+// const navigate = path => {
+// }
+//
+// const cleanUrl = path => {
+//   const current = window.location.href
+//   if(current.startsWith)
+//   const newUrl = current
+//   window.location.href = current.replace()
+// }
